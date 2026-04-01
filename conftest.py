a2695761fb48917712633b5720fdf48e691b5f29
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2026-04-02 00:07:36 +0300

    add secret pass env

--- conftest.py
+++ conftest.py
@@ -1,8 +1,6 @@
 import time
-
 import pytest
-
-import utils.secret_config
+# import utils.secret_config
 
 
 @pytest.fixture(scope="session")
@@ -36,6 +34,7 @@ def login_set_up(set_up):
     # page.fill('input:below(:text("Email"))', "symon.storozhenko@gmail.com")
     # page.press("[data-testid='siteMembers.container'] >> input[type='email']", "Tab")
     # page.fill("input[type='password']", utils.secret_config.PASSWORD)
+    # page.fill("input[type='password']", os.environ["PASSWORD"])
     # page.click("[data-testid='submit'] >> [data-testid='buttonElement']")
 
     yield page
