7e42f69fc48221bda760d10bd41af690f88fe21f
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2026-04-01 23:37:33 +0300

    correct headless cases

--- tests_ui_layout/other_thing.py
+++ tests_ui_layout/other_thing.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect, Page
 
 
 def test_run(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.set_default_timeout(15000)
